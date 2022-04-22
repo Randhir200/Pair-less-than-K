63976ff19978ca3ee739465c6a1fa285976ca140
Randhir <mail.randhirkr@gmail.com>
2022-04-22 08:57:33 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var n = 5;
+var arr = [1, 2, 3, 4, 5];
+var k = 7;
+
+// sliding window technique
+
+var left=0;
+var right=n-1;
+var max=-1;
+while(left<n&&right>=0){
+    var sum = arr[left]+arr[right];
+    if(sum<k){
+        right--;
+        if(sum>max){
+            max=sum
+        }
+    }else{
+        left++;
+    }
+}
+
+console.log(max);
